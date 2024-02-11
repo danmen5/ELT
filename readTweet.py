36e0b641ba4e061f3ae426cb2a72fdf32619ad66
Daniel M <d.menendez308@uniandes.edu.co>
2024-02-10 19:49:00 -0500

    some changes to the readTweet code

--- readTweet.py
+++ readTweet.py
@@ -1,7 +1,7 @@
 import tweepy
 import csv
 
-class TwitterReader:
+class readTweet:
     #Creates attrributes fo the tweeter conection
     def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
         self.consumer_key = consumer_key
@@ -28,11 +28,19 @@ class TwitterReader:
                 writer.writerows(tweet_data)
             
             print("Tweets retrieved and saved to 'tweets.csv' successfully.")
-        except tweepy.TweepError as e:
+        except tweepy.TweepyException as e:
             print("Error: Unable to retrieve tweets -", e)
 
 # Example usage:
-#reader = TwitterReader(consumer_key, consumer_secret, access_token, access_token_secret)
-#start_date = "2024-01-01"
-#end_date = "2024-01-31"
-#reader.read_tweets(start_date, end_date, count=5)
+consumer_key='H8B5EIhXmAbPW7b1RqTZeQRA9'
+consumer_key_secret='pcYok0Hqx7ilN71DIeb6T5xYURysqaSuekzgpN1Xyv213sk6jz'
+access_token='292996197-JU0ntacCnnusBZGTcbRmAY1YpjhyfTlQONQL4itn'
+access_token_secret='eLL4Xxor2kuAE1v3mncJM8dIl553o3ail9JuyxhThk0oO'
+reader = readTweet(consumer_key, consumer_key_secret, access_token, access_token_secret)
+start_date = "2024-01-01"
+end_date = "2024-01-31"
+reader.read_tweets(start_date, end_date, count=5)
+            
+#print(
+#    len('''Academic purposes, teach people how to build an ELT that reads tweets and store them in a warehouse. No commercial use will come from using the API and it won't be used for long ''')
+#)
