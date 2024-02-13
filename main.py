9a73f2dd6ba65c33a0350a6135a04c7e046fb6ce
Daniel M <d.menendez308@uniandes.edu.co>
2024-02-12 20:43:59 -0500

    removing snowflake credentials

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from s3 import S3Uploader
 from snowflakeRunner import snowflakeRunner
 from readTweet import readTweet
+from datetime import date
 def main():
     try:
         #Creates the connection with tweeter
@@ -13,35 +14,35 @@ def main():
         pass
 
     #Creates the connection with s3
-    access_key_id='AKIA3FLDXHL3GHXHBOH3'
-    secret_access_key='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'
-    bucket_name='snowflake-danmen'
+    access_key_id='*********'
+    secret_access_key='*******'
+    bucket_name='********'
     uploader = S3Uploader(access_key_id, secret_access_key, bucket_name)
 
     #Creates the connection with snowflake
     snowflakeConnector = snowflakeRunner(
-        account='mfb94343.us-east-1',
-        user='DANMEN5',
-        password='Efatnas10',
-        warehouse='COMPUTE_WH',
-        database='PROJECTS',
-        schema='BRONZE'
+        account='******',
+        user='******',
+        password='******',
+        warehouse='******',
+        database='******',
+        schema='*******'
     )
 
     # Connect to Snowflake
     snowflakeConnector.connect()
 
     # Execute SQL query from file
-    query_file_path = 'maxDate.sql'  
+    query_file_path = 'maxDate.sql'
+    #Builds the start date to query tweets from the last tweet  
     start_date=snowflakeConnector.execute_query_from_file(query_file_path)
-    print(start_date)
+    #Builds today's date as end date
+    end_date=date.today()
 
     # Disconnect from Snowflake
     snowflakeConnector.disconnect()
 
     #Extracts the tweets
-    start_date = "2024-01-01"
-    end_date = "2024-01-31"
     try:
         reader.read_tweets(start_date, end_date, count=5)
     except:
