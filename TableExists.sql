25be0895020a3111f89aef81d704fe5c48a4cdff
Daniel M <d.menendez308@uniandes.edu.co>
2024-02-17 19:51:45 -0500

    env variables inside sql code

--- TableExists.sql
+++ TableExists.sql
@@ -1,14 +1,18 @@
 BEGIN 
+-- Load the AWS credentials from environment variables
+CREATE OR REPLACE VARIABLE AWS_KEY_ID VARCHAR DEFAULT SYSTEM$ENCRYPT_AES('YOUR_AWS_KEY_ID');
+CREATE OR REPLACE VARIABLE AWS_SECRET_KEY VARCHAR DEFAULT SYSTEM$ENCRYPT_AES('YOUR_AWS_SECRET_KEY');
+
 -- Creates the stage with s3 in case it doesn't exist
 CREATE OR REPLACE STAGE PROJECTS.STAGES.S3
-  URL = 's3://snowflake-danmen'  -- Replace with the URL of your S3 bucket and path
-  CREDENTIALS = (AWS_KEY_ID='AKIA3FLDXHL3GHXHBOH3' AWS_SECRET_KEY='R7367oJO5yaOQkBCSUHWz6FgtKcR4n8mTxagzn8m');
+  URL = 's3://snowflake-danmen'  
+  CREDENTIALS = (AWS_KEY_ID=SYSTEM$DECRYPT_AES(AWS_KEY_ID) AWS_SECRET_KEY=SYSTEM$DECRYPT_AES(AWS_SECRET_KEY));
 
 -- checks if the table exists. If it does, it inserts the data from s3. If it doesn't, it creates the table and then inserts
 IF (EXISTS(SELECT * FROM PROJECTS.INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME='TWEETS'))
 THEN 
   COPY INTO PROJECTS.BRONZE.TWEETS
-  FROM @PROJECTS.STAGES.S3/tweets.csv  -- Replace with the path to your CSV file in your stage
+  FROM @PROJECTS.STAGES.S3/tweets.csv 
   FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1);
   RETURN 'INSERTED';
 ELSE 
